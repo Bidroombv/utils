4a7b6a0136b3cb192494b2f5dc87abfa65c2061b
Michal Mazurek <michal.mazurek@bidroom.com>
2020-02-13 15:45:58 +0100

    Add test/log

---
+++ test/log.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"bytes"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Log struct {
+	sync.Mutex
+	buf bytes.Buffer
+}
+
+func (l *Log) Write(data []byte) (n int, err error) {
+	l.Lock()
+	defer l.Unlock()
+
+	return l.buf.Write(data)
+}
+
+func (l *Log) Reset() {
+	l.Lock()
+	defer l.Unlock()
+
+	l.buf.Reset()
+}
+
+func (l *Log) Bytes() []byte {
+	l.Lock()
+	defer l.Unlock()
+
+	return l.buf.Bytes()
+}
+
+func (l *Log) String() string {
+	l.Lock()
+	defer l.Unlock()
+
+	return l.buf.String()
+}
+
+func (l *Log) Fixture(t *testing.T) {
+	t.Helper()
+
+	l.Lock()
+	defer l.Unlock()
+
+	FixtureExtra(t, "log", l.buf.String())
+}
+
+func (l *Log) Contains(t *testing.T, substr string) {
+	t.Helper()
+
+	l.Lock()
+	defer l.Unlock()
+
+	assert.True(t, strings.Contains(l.buf.String(), substr))
+}
+
+func (l *Log) DoesntContain(t *testing.T, substr string) {
+	t.Helper()
+
+	l.Lock()
+	defer l.Unlock()
+
+	assert.False(t, strings.Contains(l.buf.String(), substr))
+}
