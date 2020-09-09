c98fc96c800429ffc97d7d250563cb8df33d9047
sebastianbojara <sebastian.bojara@gmail.com>
2020-09-09 15:17:15 +0200

    HOT-116-encryptor-decryptor

--- crypton.go
+++ crypton.go
@@ -18,30 +18,30 @@ func decodeBase64(s string) []byte {
 	return data
 }
 
-func Encrypt(iv []byte, key []byte, plain string) string {
+func Encrypt(iv []byte, key []byte, plain string) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	plainBytes := []byte(plain)
-	cfb := cipher.NewCTR(block, iv)
+	ctr := cipher.NewCTR(block, iv)
 	ciphertext := make([]byte, len(plainBytes))
-	cfb.XORKeyStream(ciphertext, plainBytes)
+	ctr.XORKeyStream(ciphertext, plainBytes)
 
-	return encodeBase64(ciphertext)
+	return encodeBase64(ciphertext), nil
 }
 
-func Decrypt(iv []byte, key []byte, encrypted string) string {
+func Decrypt(iv []byte, key []byte, encrypted string) (string, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	ciphertext := decodeBase64(encrypted)
-	cfb := cipher.NewCTR(block, iv)
+	ctr := cipher.NewCTR(block, iv)
 	plaintext := make([]byte, len(ciphertext))
-	cfb.XORKeyStream(plaintext, ciphertext)
+	ctr.XORKeyStream(plaintext, ciphertext)
 
-	return string(plaintext)
+	return string(plaintext), nil
 }
