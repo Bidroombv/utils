457f96ce5c015f0c95f17613a6e8b8dbc85373df
Kamil Łopata <kamil.lopata@bidroom.com>
2020-11-20 12:04:46 +0100

    CORE-1035-mark-pointer-functions-as-deprecated

--- pointers.go
+++ pointers.go
@@ -2,6 +2,7 @@ package utils
 
 import "errors"
 
+// Deprecated, will be deleted according to the plan https://bidroom.atlassian.net/wiki/spaces/CP/pages/1386971179/Golang+PtrType+TypePtr+conversions
 func StringPtr(s string) *string {
 	if s == "" {
 		return nil
@@ -10,6 +11,7 @@ func StringPtr(s string) *string {
 	return &s
 }
 
+// Deprecated, will be deleted according to the plan https://bidroom.atlassian.net/wiki/spaces/CP/pages/1386971179/Golang+PtrType+TypePtr+conversions
 func PtrString(s *string) string {
 	if s == nil {
 		return ""
@@ -17,12 +19,14 @@ func PtrString(s *string) string {
 	return *s
 }
 
+// Deprecated, will be deleted according to the plan https://bidroom.atlassian.net/wiki/spaces/CP/pages/1386971179/Golang+PtrType+TypePtr+conversions
 func IntPtr(i int) *int {
 	return &i
 }
 
 var errIntCannotBeNil = errors.New("int cannot be nil")
 
+// Deprecated, will be deleted according to the plan https://bidroom.atlassian.net/wiki/spaces/CP/pages/1386971179/Golang+PtrType+TypePtr+conversions
 func PtrInt(i *int) (int, error) {
 	if i == nil {
 		return 0, errIntCannotBeNil
