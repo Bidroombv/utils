22bf628c7f4128539fe56dab90579e68beb616d3
Tomek Cejner <tomek@bidroom.com>
2021-03-01 14:03:00 +0100

    CORE-1459 simple gRPC logging interceptor (#8)
    * gRPC logging interceptor
    
    * Group imports
    
    Co-authored-by: Tomek Cejner <tomasz.cejner@bidroom.com>

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Utilities
 
+## gRPC logging interceptor for Zerolog
+
+Automagically logs gRPC calls via global instance of Zerolog. In case of error response, context or error is also logged.
+
+### Usage
+
+Interceptor is implemented as `ServerOption`. Find the place where gRPC server is created and simply add call to
+`logging.UnaryInterceptor` imported from this repository.
+
+```go
+server := grpc.NewServer(
+			logging.UnaryInterceptor(),
+		)
+```
+
+### Example output
+
+```
+2021-03-01T11:10:59Z ERR pkg/logger/grpc.go:48 > gRPC error="rpc error: code = NotFound desc = Id: " code=NotFound details=[] duration=73.929187 ip=127.0.0.1:51444 method=GetPropertyDetails service=inventory.v1.Inventory
+2021-03-01T11:11:15Z INF pkg/logger/grpc.go:52 > gRPC duration=0.038067 ip=127.0.0.1:51444 method=ListCodes service=inventory.v1.InventorySearch
+```
+
 ## Contributing
 
 Install **golangci-lint** to run build locally:
---
+++ logging/grpc.go
@@ -0,0 +1,59 @@
+package logger
+
+import (
+	"context"
+	"path"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
+)
+
+const logMessage = "gRPC"
+
+func logIncomingCall(ctx context.Context, event *zerolog.Event, method string, start time.Time, req interface{}) {
+	event.Time("time", start).
+		Str("service", path.Dir(method)[1:]).
+		Str("method", path.Base(method)).
+		Dur("duration", time.Since(start))
+	logIP(ctx, event)
+}
+
+func logIP(ctx context.Context, logger *zerolog.Event) {
+	if p, ok := peer.FromContext(ctx); ok {
+		*logger = *logger.Str("ip", p.Addr.String())
+	}
+}
+
+func logStatusError(logger *zerolog.Event, err error) {
+	statusErr := status.Convert(err)
+	*logger = *logger.Err(err).Str("code", statusErr.Code().String()).Str("message", statusErr.Message()).Interface("details", statusErr.Details())
+}
+
+// UnaryInterceptor is a gRPC Server Option that uses NewUnaryServerInterceptor() to log gRPC Requests.
+// It uses global zerolog instance for logging.
+func UnaryInterceptor() grpc.ServerOption {
+	return grpc.UnaryInterceptor(NewUnaryLoggingInterceptor())
+}
+
+func NewUnaryLoggingInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		startTime := time.Now()
+		resp, err := handler(ctx, req)
+		if err != nil {
+			event := log.Error()
+			logIncomingCall(ctx, event, info.FullMethod, startTime, req)
+			logStatusError(event, err)
+			event.Msg(logMessage)
+		} else {
+			event := log.Info()
+			logIncomingCall(ctx, event, info.FullMethod, startTime, req)
+			event.Msg(logMessage)
+		}
+
+		return resp, err
+	}
+}
